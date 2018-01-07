57c9cccdbab29df94778d08337a2ed6e09546c28
Thanh Son Nguyen Ngoc <nguyenngocthanhson@gmail.com>
2018-01-07 10:34:28 +0200

    Added notification to main page.

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,13 @@ export default class App extends Component {
     return (
       <div>
         <SearchBar />
+        <div className="row">
+          <div className="col-md-12">
+            <div className="alert alert-info">
+                Content of this side is updated continuously. As the author is pushing code to his Github repository: https://github.com/sinivuori/redux-music-search-prototype
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

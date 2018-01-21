56123ec515c46c53ee9f6e3132999287a8b077bc
Thanh Son Nguyen Ngoc <nguyenngocthanhson@gmail.com>
2018-01-21 16:36:13 +0200

    Added site under construction text and fontawesome icons.

--- src/components/app.js
+++ src/components/app.js
@@ -9,9 +9,10 @@ export default class App extends Component {
         <div className="row" id="notification-container">
           <div className="col-md-12">
             <div className="alert alert-info">
-              <p>Source code of site can be found at <a href="https://github.com/sinivuori/redux-music-search-prototype">this Github repository</a>.</p>
-              <p>Music search API is provided courtesy of <a href="https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/">iTunes API</a>.</p>
-              <p>Preview of songs is provided courtesy of <a href="https://www.apple.com/itunes/">iTunes</a>.</p>
+              <p><i className="fas fa-spinner"></i>&nbsp;&nbsp;&nbsp;Site is under construction.</p>
+              <p><i className="fas fa-code"></i>&nbsp;&nbsp;&nbsp;Source code can be found at <a href="https://github.com/sinivuori/redux-music-search-prototype">this <i className="fab fa-github"></i> repository</a>.</p>
+              <p><i className="fas fa-music"></i>&nbsp;&nbsp;&nbsp;API is provided courtesy of <a href="https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/">iTunes API</a>.</p>
+              <p><i className="fas fa-music"></i>&nbsp;&nbsp;&nbsp;Preview of songs is provided courtesy of <a href="https://www.apple.com/itunes/">iTunes</a>.</p>
             </div>
           </div>
         </div>

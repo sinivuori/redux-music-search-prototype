2a34bbf934c1c78e99641d2767d4c7d7a69d9ee9
Thanh Son Nguyen Ngoc <nguyenngocthanhson@gmail.com>
2018-01-21 17:53:32 +0200

    Used bootstrap resources from npm package instead of online cdn.

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import SearchBar from '../containers/search_bar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class App extends Component {
   render() {

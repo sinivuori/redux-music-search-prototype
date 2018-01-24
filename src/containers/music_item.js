6d088fbae91d28e713bfc0da9e8a664a2c944a4e
Thanh Son Nguyen Ngoc <nguyenngocthanhson@gmail.com>
2018-01-24 09:08:00 +0200

    Layout tweaks for music item.

--- src/containers/music_item.js
+++ src/containers/music_item.js
@@ -4,25 +4,28 @@ const MusicItem = ({music}) => {
   return (
     <div className="row music-item">
       <div className="col-md-2">
-        <img src={music.artworkUrl100} alt=""/>
+        <img src={music.artworkUrl60} alt={music.trackName} className="rounded" />
       </div>
       <div className="col-md-2">
-        <strong>Name:</strong> {music.trackName}
+        <div className="row music-info-text">
+          <strong>{music.trackName}</strong>
+        </div>
+        <div className="row music-info-text">
+          <p><strong>By:</strong></p>
+          <p>{music.artistName}</p>
+        </div>
       </div>
-      <div className="col-md-2">
-        <strong>Artist:</strong> {music.artistName}
-      </div>
-      <div className="col-md-3">
+      <div className="col-md-5">
         <audio controls>
           <source src={music.previewUrl}/>
         </audio>
       </div>
       <div className="col-md-2">
-        <div className="row">
-          <strong>Preview of song is provided courtesy of iTunes <i className="far fa-heart"></i> <i className="fab fa-itunes"></i></strong>
+        <div className="row music-info-text">
+          <strong>Preview of this song is provided courtesy of iTunes</strong>
         </div>
-        <div className="row">
-          <a href={music.trackViewUrl}>Purchase at iTunes Store</a>
+        <div className="row music-info-text">
+          <a href={music.trackViewUrl}>Purchase at iTunes Store <i className="fab fa-itunes"></i></a>
         </div>
       </div>
     </div>

8b1dc68129888d00b2c9122527a3aec0cda77aa3
Thanh Son Nguyen Ngoc <nguyenngocthanhson@gmail.com>
2018-02-11 11:15:17 +0200

    Removed music player after a new search is performed.

--- src/reducers/reducer_select_music.js
+++ src/reducers/reducer_select_music.js
@@ -1,10 +1,14 @@
 import {SELECT_MUSIC} from '../actions/action_select_music';
+import {FETCH_MUSIC} from '../actions/action_fetch_music';
 
 export default function(state = null, action) {
   var newState = {};
   switch (action.type) {
     case SELECT_MUSIC:
       newState = action.payload;
+      break;
+    case FETCH_MUSIC:
+      newState = null;
       break;
     default:
       newState = state;

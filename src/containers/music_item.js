b280f6f06f84e85ecb56bfd946fa4dd110989233
Thanh Son Nguyen Ngoc <nguyenngocthanhson@gmail.com>
2018-02-11 03:14:09 +0200

    Updated styling for play icon.

--- src/containers/music_item.js
+++ src/containers/music_item.js
@@ -7,17 +7,23 @@ class MusicItem extends Component {
   render() {
     return (
       <div className="row music-item">
-        <div className="col-md-2 music-info-text">
-          <img src={this.props.music.artworkUrl100} alt={this.props.music.trackName} className="rounded" />
+        <div className="col-md-2 col-xs-2 music-info-text">
+          <img src={this.props.music.artworkUrl100} alt={this.props.music.trackName} className="music-thumbnail rounded" onClick={() => this.props.selectMusic(this.props.music)} />
+          <div className="play-icon-positioner">
+            <div className="play-icon-container">
+              <i className="fas fa-play fa-3x"></i>
+            </div>
+          </div>
         </div>
-        <div className="col-md-10">
+        <div className="col-md-10 col-xs-10">
           <div className="row">
-            <div className="col-md-2 col-xs-12 music-info-text"><button className="btn btn-lg" onClick={() => this.props.selectMusic(this.props.music)}><i className="far fa-play-circle fa-2x"></i></button></div>
-            <div className="col-md-4">
+            <div className="col-md-12">
               <div className="music-info-text">
                 <strong>{this.props.music.trackName}</strong>
               </div>
             </div>
+          </div>
+          <div className="row">
             <div className="col-md-4">
               <div className="music-info-text">
                 <p><strong>By:</strong> {this.props.music.artistName}</p>
